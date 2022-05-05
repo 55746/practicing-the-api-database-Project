dc788fa410f67ed3fa89be0bd6a98c6c614e0ccf
55746 <Lawhead.gavin@live.jobcorps.org>
2022-05-05 16:08:43 +0000

    waiting for teacher

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
@@ -15,29 +16,62 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config["JWT_SECRET_KEY"] = "super-secret"
+jwt = JWTManager(app)
+
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    is_user = User.query.filter_by(email='email')
+
+    if is_user.email != "email" or is_user.password != "password":
+        return jsonify({"msg": "Bad username or password DumbFuck"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    user_query = User.query.all()
+    all_user = list(map(lambda x: x.serialize(), user_query))
+    return jsonify(all_user), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/user/<int:user_id>', methods=['GET'])
+def userID(user_id):
+    user = User.query.get(user_id)
+    return jsonify(user.email), 200
+# YOU HAVE TO HAVE SERIALIZE BECAUSE OF WHAT ITS RETURNING
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
